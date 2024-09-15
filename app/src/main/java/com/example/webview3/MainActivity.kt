e15f9b6fee901b5bf5723be246911f8912c2316e
victorgeorgiev <ec22752@qmul.ac.uk>
2024-09-16 00:49:54 +0100

    Integrated SDKs and added error alert dialogue.

--- app/src/main/java/com/example/webview3/MainActivity.kt
+++ app/src/main/java/com/example/webview3/MainActivity.kt
@@ -4,25 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.webview3.ui.theme.WebView3Theme
+import com.facebook.ads.AudienceNetworkAds
+//import com.google.android.gms.ads.MobileAds;
+import com.appsflyer.AppsFlyerLib;
+import com.onesignal.OneSignal;
+
 
 class MainActivity : ComponentActivity() {
+    private val AF_DEV_KEY = "ВАШИЯТ_DEV_KEY"
+    private val ONESIGNAL_APP_ID = "ВАШИЯТ_APP_ID"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AudienceNetworkAds.initialize(this)
+        //MobileAds.initialize(this);
+        AppsFlyerLib.getInstance().init(AF_DEV_KEY, null, this);
+        AppsFlyerLib.getInstance().start(this);
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
         enableEdgeToEdge()
         setContent {
             WebView3Theme {
-                Column(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.statusBars.asPaddingValues())
+                ) {
                     WebViewScreen()
                 }
             }
--- app/src/main/java/com/example/webview3/WebView.kt
+++ app/src/main/java/com/example/webview3/WebView.kt
@@ -2,10 +2,14 @@ package com.example.webview3
 
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -21,6 +25,15 @@ import androidx.compose.ui.viewinterop.AndroidView
 fun WebViewScreen() {
     val context = LocalContext.current
     val webViewState = remember { mutableStateOf(WebView(context)) }
+    var showAlert by remember {
+        mutableStateOf(false)
+    }
+    var errorName by remember {
+        mutableStateOf("")
+    }
+    var errorText by remember {
+        mutableStateOf("")
+    }
 
     // Enable JavaScript and other settings
     LaunchedEffect(Unit) {
@@ -33,18 +46,39 @@ fun WebViewScreen() {
                     request: WebResourceRequest,
                     error: WebResourceError
                 ) {
-                    println("ERRRROOOR")
+                    showAlert = true
+                    errorName = "Error code ${error.errorCode}"
+                    errorText = error.description.toString()
+                }
+
+                override fun onReceivedHttpError(
+                    view: WebView,
+                    request: WebResourceRequest,
+                    errorResponse: WebResourceResponse
+                ) {
+                    showAlert = true
+                    errorName = "Error code ${errorResponse.statusCode}"
+                    errorText = errorResponse.responseHeaders.toString()
                 }
 
                 override fun onPageFinished(view: WebView?, url: String?) {
-                    // Page loading finished
-                    println("LOADED???")
                 }
             }
             loadUrl("https://youtube.com")
         }
     }
-// Handle back button press
+    if (showAlert) {
+        AlertDialog(
+            onDismissRequest = { showAlert = false },
+            confirmButton = {
+                Button(onClick = { showAlert = false }) {
+                    Text(text = "Okay")
+                }
+            },
+            title = { Text(text = errorName) },
+            text = { Text(text = errorText) })
+    }
+    // Handle back button press
     var canGoBack by remember { mutableStateOf(false) }
     BackHandler(enabled = canGoBack) {
         webViewState.value.goBack()
